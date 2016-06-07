25876d9c62c61186d11b7d0a5e982e1c6afb6299
Samuel Giddins <segiddins@segiddins.me>
2016-06-06 20:39:31 -0500

    [Installer] Just rescue LoadError

--- lib/postit/installer.rb
+++ lib/postit/installer.rb
@@ -6,7 +6,7 @@ module PostIt
 
     def installed?
       !Gem::Specification.find_by_name('bundler', @bundler_version).nil?
-    rescue Gem::MissingSpecVersionError
+    rescue LoadError
       false
     end
 
